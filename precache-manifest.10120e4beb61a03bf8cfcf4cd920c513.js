a97274c37b734105b0334c115d8158831c536c64
Iagor Sousa <54728248+IagorSs@users.noreply.github.com>
2020-11-08 16:05:26 -0300

    Updates

---
+++ precache-manifest.10120e4beb61a03bf8cfcf4cd920c513.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9719d7a02d4723651ddeb3f5f51fc0bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6320ddc49baab5466a00",
+    "url": "/static/css/main.3502e27f.chunk.css"
+  },
+  {
+    "revision": "dbc337882e9edca5bbf7",
+    "url": "/static/js/2.a610d416.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a610d416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6320ddc49baab5466a00",
+    "url": "/static/js/main.7a2e6b53.chunk.js"
+  },
+  {
+    "revision": "61315fc713342d285b8b",
+    "url": "/static/js/runtime-main.81cbfe41.js"
+  }
+]);
